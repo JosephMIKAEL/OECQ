19904fd6bae3707780854d02bc5bc508f87681e0
Noot <nathan.github22@gmail.com>
2024-07-08 15:51:20 +0200

    Traduction footer

--- js/date.js
+++ js/date.js
@@ -5,4 +5,4 @@
 let dateYear = new Date().getFullYear()
 let copyright = document.querySelector(".copyright")
 
-copyright.textContent = `\u00A9 ${dateYear} OECQ. Tous droits réservés.`
+copyright.textContent = `\u00A9 ${dateYear} OECQ. All Rights Reserved.`
